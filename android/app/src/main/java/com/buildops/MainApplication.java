c95ac3a5da113bd2d4a194585ee1a111df2ded8a
Mac <mohammed.ali.chherawalla@gmail.com>
2019-12-03 18:34:20 +0530

    android build
    Author:    Mac <mohammed.ali.chherawalla@gmail.com>
    Date:      Tue Dec 3 18:34:20 2019 +0530

--- android/app/src/main/java/com/buildops/MainApplication.java
+++ android/app/src/main/java/com/buildops/MainApplication.java
@@ -7,7 +7,6 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
